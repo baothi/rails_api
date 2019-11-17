2ad3ba9f72669036791984205dcb2825d65ca8c9
baothi <baothi246@gmail.com>
2019-11-16 20:31:20 -0800

    create update review

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,1 +1,65 @@
-reviews_controller.rb
+class Api::V1::ReviewsController < ApplicationController
+  before_action :load_book, only: :index
+  before_action :load_review, only: [:show, :update, :destroy]
+  before_action :authenticate_with_token!, only: [:create, :update, :destroy]
+
+  def index
+    @review = @book.reviews
+    json_response "index reviews successfully", true, {reviews: @reviews}, :ok
+  end
+
+  def show
+    json_response "Show reviews successfully", true, {review: @review}, :ok
+  end
+
+  def create
+    review = Review.new review_params
+    review.user_id = current_user.id
+    review.book_id = params[:book_id]
+    if review.save
+      json_response "Create reviewsuccessfully", true, {review: review}, :ok
+    else
+      json_response "Create review fail", false, {}, :unproccessable_entity
+    end
+  end
+
+  def update
+    if correct_user @review.user
+      if @review.update review_params
+        json_response "update review successfully", true, {review: @review}, :ok
+      else
+        json_response "Update review fail", false, {}, :unproccessable_entity
+      end
+    else
+      json_response "You dont have permission to do this", false, {}, :unauthorized
+    end
+  end
+
+  def destroy
+    if correct_user @review.user
+      if @review.destroy
+        json_response "Deleted review successfully", true, {review: @review}, :ok
+      else
+        json_response "Deleted review fail", false, {}, :unproccessable_entity
+      end
+    else
+      json_response "You dont have permission to do this", false, {}, :unauthorized
+    end
+  end
+
+  private
+  def load_book
+    @book = Book.find_by_id params[:book_id]
+  end
+
+  def load_review
+    @review = Review.find_by_id params[:id]
+    unless @review.present?
+      json_response "Cannot find a review", false, {}, :not_found
+    end
+  end
+
+  def review_params
+    params.require(:review).permit :title, :content_rating, :recommend_rating
+  end
+end
